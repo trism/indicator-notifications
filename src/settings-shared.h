d24e9db303ce29f1f61aa66ca9e43b3ab17984d2
Jason Conti <jason.conti@gmail.com>
2011-05-08 15:38:05 -0400

    Stripped settings

--- src/settings-shared.h
+++ src/settings-shared.h
@@ -23,46 +23,5 @@ with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define __EXAMPLE_SETTINGS_SHARED_H__
 
 #define SETTINGS_INTERFACE              "com.example.indicator.example"
-#define SETTINGS_SHOW_CLOCK_S           "show-clock"
-#define SETTINGS_TIME_FORMAT_S          "time-format"
-#define SETTINGS_SHOW_SECONDS_S         "show-seconds"
-#define SETTINGS_SHOW_DAY_S             "show-day"
-#define SETTINGS_SHOW_DATE_S            "show-date"
-#define SETTINGS_CUSTOM_TIME_FORMAT_S   "custom-time-format"
-#define SETTINGS_SHOW_CALENDAR_S        "show-calendar"
-#define SETTINGS_SHOW_WEEK_NUMBERS_S    "show-week-numbers"
-#define SETTINGS_SHOW_EVENTS_S          "show-events"
-#define SETTINGS_SHOW_LOCATIONS_S       "show-locations"
-#define SETTINGS_LOCATIONS_S            "locations"
-#define SETTINGS_TIMEZONE_NAME_S        "timezone-name"
-
-enum {
-	SETTINGS_TIME_LOCALE = 0,
-	SETTINGS_TIME_12_HOUR = 1,
-	SETTINGS_TIME_24_HOUR = 2,
-	SETTINGS_TIME_CUSTOM = 3
-};
-
-/* TRANSLATORS: A format string for the strftime function for
-   a clock showing 12-hour time without seconds. */
-#define DEFAULT_TIME_12_FORMAT   N_("%l:%M %p")
-
-/* TRANSLATORS: A format string for the strftime function for
-   a clock showing 24-hour time without seconds. */
-#define DEFAULT_TIME_24_FORMAT   N_("%H:%M")
-
-#define DEFAULT_TIME_FORMAT          DEFAULT_TIME_12_FORMAT
-#define DEFAULT_TIME_FORMAT_WITH_DAY DEFAULT_TIME_12_FORMAT_WITH_DAY
-
-/* TRANSLATORS: A format string for the strftime function for
-   a clock showing the day of the week and the time in 12-hour format without
-   seconds. */
-#define DEFAULT_TIME_12_FORMAT_WITH_DAY N_("%a %l:%M %p")
-
-/* TRANSLATORS: A format string for the strftime function for
-   a clock showing the day of the week and the time in 24-hour format without
-   seconds.  Information is available in this Launchpad answer:
-   https://answers.launchpad.net/ubuntu/+source/indicator-datetime/+question/149752 */
-#define DEFAULT_TIME_24_FORMAT_WITH_DAY N_("%a %H:%M")
 
 #endif
