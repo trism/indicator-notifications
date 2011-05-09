4b01cb8c06fb5e033eb99f54486f1e779f671cac
Jason Conti <jason.conti@gmail.com>
2011-05-09 15:05:26 -0400

    Moving the local install to /home/jconti/Projects/indicators/example/build.

--- indicator-install.sh
+++ indicator-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PREFIX_NAME="$HOME/Projects/indicator-example/build"
+PREFIX_NAME="$HOME/Projects/indicators/example/build"
 
 cp -v "$PREFIX_NAME/share/dbus-1/services/indicator-example.service" "/usr/share/dbus-1/services/"
 cp -v "$PREFIX_NAME/indicators/2/libexample.so" "/usr/lib/indicators/5/"
--- localgen.sh
+++ localgen.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-PREFIX_NAME="$HOME/Projects/indicator-example/build"
+PREFIX_NAME="$HOME/Projects/indicators/example/build"
 
 ./autogen.sh --prefix="$PREFIX_NAME" --libdir="$PREFIX_NAME" --enable-localinstall
