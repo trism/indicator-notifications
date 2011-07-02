787e8de6fc59bf37ae82e3ff827a94d8f7d75390
Jason Conti <jason.conti@gmail.com>
2011-07-02 14:43:17 -0400

    * Updated testing scripts for new gtk3 indicator.

--- indicator-uninstall.sh
+++ indicator-uninstall.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 rm -v "/usr/share/dbus-1/services/indicator-notifications.service"
-rm -v "/usr/lib/indicators/5/libnotifications.so"
+rm -v "$(pkg-config --variable=indicatordir indicator3)/libnotifications.so"
